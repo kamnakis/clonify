ea32801d29f373601c01220789f219c5cd073752
Stelios Kamnakis <kamnis1997@gmail.com>
2021-01-11 01:43:31 +0200

    name change

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'spotify-clone',
+    title: 'Clonify',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=0' },
